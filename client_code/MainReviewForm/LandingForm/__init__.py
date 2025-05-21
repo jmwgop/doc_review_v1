596bd43b9f1262d5e3ba55f5f2b9a5b5f15277c4
jmwgop@gmail.com <jmwgop@gmail.com>
2025-05-21 19:56:08 +0000

    Edited MainReviewForm.LandingForm, settings, ReviewService and MainReviewForm

--- client_code/MainReviewForm/LandingForm/__init__.py
+++ client_code/MainReviewForm/LandingForm/__init__.py
@@ -1,13 +1,25 @@
 from ._anvil_designer import LandingFormTemplate
 from anvil import *
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import anvil.server
 
 class LandingForm(LandingFormTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    # Populate the dropdown from the server
+    try:
+      items = anvil.server.call('get_document_dropdown_items')
+    except Exception as e:
+      self.doc_status_text.text = f"Error loading documents: {e}"
+      items = []
+
+    self.doc_dropdown.items = items
+    self.doc_dropdown.selected_value = None  # No doc selected initially
+
+  def load_btn_click(self, **event_args):
+    doc_id = self.doc_dropdown.selected_value
+    if doc_id:
+      # Swap to ReviewForm, passing doc_id
+      open_form('ReviewForm', doc_id=doc_id)
+    else:
+      self.doc_status_text.text = "Please select a document before loading."
--- server_code/ReviewService.py
+++ server_code/ReviewService.py
@@ -1,17 +1,12 @@
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import anvil.server
+import anvil.tables as tables
+from anvil.tables import app_tables
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+@anvil.server.callable
+def get_document_dropdown_items():
+  """
+    Returns a list of (label, value) tuples for the dropdown.
+    """
+  docs = app_tables.documents.search()
+  # Use doc_id for both label and value; you could add more here if you want
+  return [(d["doc_id"], d["doc_id"]) for d in docs]
